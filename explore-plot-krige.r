5159928bf7a2a491324f02ef69c28fc3f616472a
anitameh <anita_mehrotra@yahoo.com>
2014-05-04 19:58:55 -0400

    added images etc

--- explore-plot-krige.r
+++ explore-plot-krige.r
@@ -1,8 +1,7 @@
 #############################
-# @final-project: Part 1
+# @final-project
 # @course: Spatial Statistics
 # @author: Anita Mehrotra
-# @date: April 13, 2014
 #############################
 
 # load necessary packages
@@ -67,14 +66,10 @@ PR = append(sort(PR_ind_to_remove)[2:lenPR], 3156)
 all_ind_to_remove = c(ind_to_remove, PR)
 shape_data = shape[-na.omit(all_ind_to_remove),]
 
-# read in mobility data (data does NOT include AK and HI)
-#data = read.csv(paste(wd, "/data/absolute-relative-mobility-county.csv", sep=""))
-
-# read in long-lat data
+# read in mobility data (data does NOT include AK and HI) & long-lat data
 alldata = read.csv(paste(wd, "data/complete_data.csv", sep=""))
 vizdata = read.csv(paste(wd, "data/viz_all_data.csv", sep=""))
-#rel_mobility = na.omit(alldata$relative)
-#abs_mobility = na.omit(alldata$absolute)
+
 abs_mobility = vizdata$absolute
 sample_size = as.numeric(alldata$sample.size)
 
@@ -105,32 +100,6 @@ proj4string(shape_data) = CRS("+proj=longlat")
 proj = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 USA_proj = spTransform(shape_data, CRS(proj))
 
-# plot data
-# plot(USA_proj)
-
-# plot relative mobility withOUT standardization, just for comparison
-#num_color = 9
-#plot_colors = brewer.pal(num_color,"YlOrRd")
-
-#class = classIntervals(rel_mobility, num_color, style="quantile")
-#colcode = findColours(class, plot_colors)
-
-#png( "relative_mobility.png", width = 5000, height = 4500 )
-#plot(USA_proj, col=colcode, border="grey", lwd=0.75)
-#legend(-78, 33, legend=names(attr(colcode, "table")), 
-#       fill=attr(colcode, "palette"), cex=5, bty="n")
-#dev.off()
-
-# plot standardized relative mobility
-#class = classIntervals(rel_mobility_std, num_color, style="quantile")
-#colcode = findColours(class, plot_colors)
-
-#png( "relative_mobility_std.png", width = 5000, height = 4500 )
-#plot(USA_proj, col=colcode, border="grey", lwd=0.75)
-#legend(-78, 33, legend=names(attr(colcode, "table")), 
-#       fill=attr(colcode, "palette"), cex=5, bty="n")
-#dev.off()
-
 # plot absolute mobility withOUT standardization, for comparison 
 num_color = 9
 plot_colors = rev(brewer.pal(num_color,"YlOrRd"))
@@ -139,7 +108,6 @@ class_abs = classIntervals(abs_mobility, num_color, style="quantile")
 colcode_abs = findColours(class_abs, plot_colors)
 
 png( "absolute_mobility.png", width = 5000, height = 4500 )
-#png( "absolute_mobility_vizdata.png", width = 5000, height = 4500 )
 plot(USA_proj, col=colcode_abs, border="grey", lwd=0.75)
 legend(-75, 35, legend=names(attr(colcode_abs, "table")), 
        fill=attr(colcode_abs, "palette"), cex=5, bty="n")
@@ -155,6 +123,11 @@ legend(-75, 35, legend=names(attr(colcode_abs, "table")),
        fill=attr(colcode_abs, "palette"), cex=5, bty="n")
 dev.off()
 
+# print some summary statistics
+print(summary(abs_mobility_std))
+new_abs = na.omit(abs_mobility_std)
+print(sd(new_abs))
+
 ## STEP 4: CHECK FOR NONSTATIONARITY
 
 # jitter coordinates
@@ -162,6 +135,30 @@ LONG = vizdata$long
 LAT = vizdata$lat
 coords = cbind(LONG, LAT)
 jittered_coords = jitterDupCoords(coords, max=0.01)
+
+# create geodata for nonstandardized data (for comparison purposes)
+abs_notstd_newdata = cbind(abs_mobility, jittered_coords)
+my_abs_notstd_geodata = as.geodata(abs_notstd_newdata, coords.col = 2:3, data.col = 1)
+
+# plot variog cloud using robust estimator
+my_abs_notstd_variog = variog(my_abs_notstd_geodata, 
+                              option="cloud", estimator.type="modulus")
+png( "absolute_mobility_notstd_cloud_unrestricted.png", 
+     width = 5000, height = 4500 )
+plot(my_abs_notstd_variog$u, my_abs_notstd_variog$v, pch=10,
+     col=rgb(red=0, green=255, blue=0, alpha=50, max=255),
+     xlab="Distance", ylab="Semivariance",
+     main = "Cressie estimator, Distance Unrestricted")
+dev.off()
+
+my_abs_notstd_variog = variog(my_abs_notstd_geodata, option="cloud", 
+                       max.dist=20, estimator.type="modulus")
+png( "absolute_mobility_notstd_cloud_restricted.png", width = 5000, height = 4500 )
+plot(my_abs_notstd_variog$u, my_abs_notstd_variog$v, pch=10,
+     col=rgb(red=0, green=255, blue=0, alpha=50, max=255),
+     xlab="Distance", ylab="Semivariance",
+     main = "Cressie estimator, Max Dist = 20")
+dev.off()
 
 # create geodata
 abs_newdata = cbind(abs_mobility_std, jittered_coords)
@@ -259,6 +256,8 @@ plot(abs_dist, abs_semivar, col=rgb(red=0, green=0, blue=255, alpha=100, max=255
 init_sill = 30000
 init_range = 20
 
+init_sill = 20000
+init_range = 20
 print("Spherical")
 spherical = variofit(my_abs_variog, c(init_sill, init_range), 
                      cov.model="spherical", weights="cressie")
@@ -317,7 +316,8 @@ abs_krige_control = krige.control(type.krige="ok", cov.model="spherical",
                                   cov.pars=c(abs_sigma2, abs_phi), nugget=abs_tau2)
 
 # predict
-abs_krige_pred = krige.conv(my_abs_geodata, locations=pred_grid, krige=abs_krige_control)
+abs_krige_pred = krige.conv(my_abs_geodata, locations=pred_grid, 
+                            krige=abs_krige_control)
 
 # plot
 image(abs_krige_pred, pred_grid, col=heat.colors(200), 
@@ -334,3 +334,25 @@ contour(abs_krige_pred, pred_grid, col="white", add=TRUE)
 legend.krige(x.leg=c(-93, -70), y.leg=c(52, 56), sqrt(abs_krige_pred$krige.var))
 
 # Step 8. remove state effect
+
+# regress on a matrix that has dim [number of counties x 48] 
+#   each element is 1 if the measurement is in that state and 0 o/w
+state_effect = read.csv(paste(wd, "data/all-ones.csv", sep=""), header=FALSE)
+linear_reg = lm(aa ~ state_effect[,1] + state_effect[,2] + state_effect[,3] +
+                  state_effect[,4] + state_effect[,5] + state_effect[,6] + 
+                  state_effect[,7] + state_effect[,8] + state_effect[,9] +
+                  state_effect[,10] + state_effect[,11] + state_effect[,12] +
+                  state_effect[,13] + state_effect[,14] + state_effect[,15] +
+                  state_effect[,16] + state_effect[,17] + state_effect[,18] + 
+                  state_effect[,19] + state_effect[,20] + state_effect[,21] +
+                  state_effect[,22] + state_effect[,23] + state_effect[,24] +
+                  state_effect[,25] + state_effect[,26] + state_effect[,27] +
+                  state_effect[,28] + state_effect[,29] + state_effect[,30] + 
+                  state_effect[,31] + state_effect[,32] + state_effect[,33] +
+                  state_effect[,34] + state_effect[,35] + state_effect[,36] +
+                  state_effect[,37] + state_effect[,38] + state_effect[,39] +
+                  state_effect[,40] + state_effect[,41] + state_effect[,42] + 
+                  state_effect[,43] + state_effect[,44] + state_effect[,45] +
+                  state_effect[,46] + state_effect[,47] + state_effect[,48])
+residuals = linear_reg$residuals
+# conduct kriging on residuals
